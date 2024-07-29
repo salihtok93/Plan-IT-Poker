bc48f3c97b976486b90e7c0c0039005a0cb97e16
Salih Tok <salih.tok.661@gmail.com>
2024-07-29 18:17:42 +0300

    hikaye ekleme compenenti eklendi gorunurde

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Dashboard from "./Pages/Dashboard";
 import Navbar from "./Components/navbar";
 // import RoomTable from "./Components/roomtable";
 // import Choice from "./Components/choice";
+// import NewStory from "./Components/newstory";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Dashboard />
-      <hr />
       {/* <Choice /> */}
       {/* <RoomTable /> */}
+      {/* <NewStory /> */}
     </div>
   );
 }
---
+++ src/Components/newstory.js
@@ -0,0 +1,74 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Yeni Hikaye Oluştur</DialogTitle>
+        <DialogContent>
+          <TextField
+            style={{
+              height: "70px",
+              width: "350px",
+            }}
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            placeholder="Hikaye metninizi buraya koyun..."
+            type="text"
+            fullWidth
+            variant="standard"
+            multiline
+            rows={3}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>İptal</Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleClickOpen}
+            type="button"
+          >
+            Kaydet
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
