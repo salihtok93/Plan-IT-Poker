c402b854cd600f6d3a7af9a26cbe7f606f62631a
Salih Tok <salih.tok.661@gmail.com>
2024-08-02 15:25:19 +0300

    Mola isteme özelliği eklendi diğer ekranlarda socket bağlantısı yapıldı

--- src/Components/snackbar.js
+++ src/Components/snackbar.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { Snackbar, Alert } from "@mui/material";
 
-function OpenSnackbar({ open, message, onClose, severity }) {
+function OpenSnackbar({ open, message, onClose, severity, position }) {
+  const anchorOrigin =
+    position === "center"
+      ? { vertical: "top", horizontal: "center" }
+      : { vertical: "bottom", horizontal: "left" };
+
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={onClose}
+      anchorOrigin={anchorOrigin}
+    >
       <Alert onClose={onClose} severity={severity} sx={{ width: "100%" }}>
         {message}
       </Alert>
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -9,6 +9,7 @@ import { PointCard } from "../Components/pointCard";
 import Usertable from "../Components/userTable";
 import { socket } from "../Services/socket";
 import Navbar from "../Components/navbar";
+import OpenSnackbar from "../Components/snackbar";
 
 const Dashboard = () => {
   const numbers = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100, "?"];
@@ -16,6 +17,19 @@ const Dashboard = () => {
 
   const handleClick = (number) => {
     console.log("Paper clicked!", number);
+  };
+  const handlePause = () => {
+    console.log("pause tıklandı");
+    socket.emit("break request");
+    // setSnackbarMessage("Mola İsteniyor");
+    // setSnackbarPosition("center");
+    // setSnackbarOpen(true);
+  };
+  const [snackbarPosition, setSnackbarPosition] = useState("bottom");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
   };
 
   useEffect(() => {
@@ -35,18 +49,26 @@ const Dashboard = () => {
       console.log("yeni kullanıcı");
       setTrigger((t) => t + 1);
     }
+    function onNotification() {
+      console.log("Mola isteği geldi");
+      setSnackbarMessage("Elmo!");
+      setSnackbarPosition("center");
+      setSnackbarOpen(true);
+    }
 
     // function onFooEvent(value) {
     //   console.log("onFooEvent ");
     // }
 
     // socket.on("error", onConnect);
+    socket.on("break notification", onNotification);
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("user list", onNewUser);
     // socket.on("message", onFooEvent);
 
     return () => {
+      socket.off("break notification", onNotification);
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
       socket.off("new user", onNewUser);
@@ -109,10 +131,16 @@ const Dashboard = () => {
                 <Typography>URL VE KOPYALAMA EKLENECEK</Typography>
               </AccordionDetails>
             </Accordion>
-            <Typography
-              variant="h6"
-              style={{ textAlign: "center" }}
-            ></Typography>
+            <hr></hr>
+            <>
+              <Button onClick={handlePause}>Mola İste</Button>
+              <OpenSnackbar
+                position={snackbarPosition}
+                open={snackbarOpen}
+                message={snackbarMessage}
+                onClose={handleSnackbarClose}
+              />
+            </>
           </Paper>
         </Grid>
       </Grid>
